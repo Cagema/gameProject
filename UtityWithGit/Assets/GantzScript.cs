2d60c57015c6d9b1a896e031b5a130304afb032b
Cagema <Cagema.erm@gmail.com>
2019-10-17 07:59:26 +0300

    Add more characters
    commit

---
+++ UtityWithGit/Assets/GantzScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GantzScript : MonoBehaviour
+{
+    public float delay;
+    private float nextChar;
+
+    public GameObject newCharacter;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.deltaTime > nextChar)
+        {
+            Instantiate(newCharacter, new Vector3(Random.Range(0, 3), Random.Range(0, 3), Random.Range(0, 3)), Quaternion.identity);
+            nextChar = Time.deltaTime + delay;
+        }
+    }
+}
